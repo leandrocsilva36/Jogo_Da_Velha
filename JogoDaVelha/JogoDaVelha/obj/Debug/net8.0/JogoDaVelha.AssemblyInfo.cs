3ce871197461fa9561ac9f38859d71e20c833061
leandrocsilva36 <leandro.c.silva36@aluno.senai.br>
2025-09-18 18:23:04 -0300

    update
    update 18/08/25

--- JogoDaVelha/JogoDaVelha/obj/Debug/net8.0/JogoDaVelha.AssemblyInfo.cs
+++ JogoDaVelha/JogoDaVelha/obj/Debug/net8.0/JogoDaVelha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6211e89fb5af3f362c8e10a783f28f9e5abcdbe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b2e2b164c052ba84b37e2d51cf75daf0b40a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- JogoDaVelha/JogoDaVelha/obj/Release/net8.0/JogoDaVelha.AssemblyInfo.cs
+++ JogoDaVelha/JogoDaVelha/obj/Release/net8.0/JogoDaVelha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6211e89fb5af3f362c8e10a783f28f9e5abcdbe5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad89951baab8d5b9dc498586457a33a969e044e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoDaVelha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
